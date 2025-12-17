5918ec7b26e707157b018ae7fb36563a57fb60d7
ywx1202661 <yuxuejiao3@h-partners.com>
2025-12-17 18:36:57 +0800

    update copyright and license
    Co-authored-by: ywx1202661<yuxuejiao3@h-partners.com>

--- src/platform/hccp/external_depends/inc/driver/hwts_task_info.h
+++ src/platform/hccp/external_depends/inc/driver/hwts_task_info.h
@@ -6,7 +6,24 @@
  * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
  * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
  * See LICENSE in the root of the software repository for the full text of the License.
+ * 
+ * The code snippet comes from Cann project.
+ * 
+ * Copyright (c) Huawei Technologies Co., Ltd. 2012-2019. All rights reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+
 #ifndef HWTS_DRV_TASK_INFO_H
 #define HWTS_DRV_TASK_INFO_H
 
