63666525801503f24adb81b380cde1a24814220e
AI-oriented_programming <xuhaoran45@huawei.com>
2026-03-03 10:47:30 +0800

    fix huge hcomm
    Co-authored-by: XuHaoran<xuhaoran45@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !655 merge fix_rdma_hcomm_pkg into master
    
    fix huge hcomm
    
    Created-by: AI-oriented_programming
    Commit-by: XuHaoran
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #123-->
    <!-- 如果这个PR不涉及Issue，可填写"NA"。-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于构造对应xx测试用例、二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/hcomm!655

--- cmake/third_party/rdma-core.cmake
+++ cmake/third_party/rdma-core.cmake
@@ -13,7 +13,7 @@ set(RDMA_CORE_NAME "rdma-core")
 set(ROOT_BUILD_PATH "${CMAKE_BINARY_DIR}")
 set(RDMA_CORE_SEARCH_PATHS "${CANN_3RD_LIB_PATH}/${RDMA_CORE_NAME}")
 set(RDMA_CORE_ROOT_DIR ${ROOT_BUILD_PATH}/rdma-core)
-set(RDMA_CORE_SRC_DIR ${ROOT_BUILD_PATH}/rdma-core/src)
+set(RDMA_CORE_SRC_DIR ${ROOT_BUILD_PATH}/rdma-core-42.7)
 set(RDMA_CORE_BUILD_DIR ${ROOT_BUILD_PATH}/rdma-core/build)
 if(POLICY CMP0135)
     cmake_policy(SET CMP0135 NEW)
@@ -26,28 +26,33 @@ else()
     set(RDMA_CORE_URL "https://gitcode.com/cann-src-third-party/rdma-core/releases/download/v42.7-h1/rdma-core-42.7.tar.gz")
     set(RDMA_CORE_PATCH_URL "https://gitcode.com/cann-src-third-party/rdma-core/releases/download/v42.7-h1/rdma-core-42.7.patch")
     file(DOWNLOAD ${RDMA_CORE_PATCH_URL} ${ROOT_BUILD_PATH}/rdma-core-42.7.patch)
-    set(FETCH_COMMAND
-        URL ${RDMA_CORE_URL}
-        DOWNLOAD_DIR ${RDMA_CORE_ROOT_DIR}
-        URL_HASH SHA256=aa935de1fcd07c42f7237b0284b5697b1ace2a64f2bcfca3893185bc91b8c74d
-        PATCH_COMMAND patch -p1 < ${ROOT_BUILD_PATH}/rdma-core-42.7.patch
+    file(DOWNLOAD ${RDMA_CORE_URL} ${ROOT_BUILD_PATH}/rdma-core-42.7.tar.gz
+        EXPECTED_HASH SHA256=aa935de1fcd07c42f7237b0284b5697b1ace2a64f2bcfca3893185bc91b8c74d
+    )
+    execute_process(
+        COMMAND tar -zxf rdma-core-42.7.tar.gz
+        WORKING_DIRECTORY ${ROOT_BUILD_PATH}
+    )
+    execute_process(
+        COMMAND patch -p1  -i ${ROOT_BUILD_PATH}/rdma-core-42.7.patch
+        WORKING_DIRECTORY ${RDMA_CORE_SRC_DIR}
     )
     message(STATUS "downloading ${RDMA_CORE_URL} to ${RDMA_CORE_ROOT_DIR}")
 endif()
-
-include(FetchContent)
-
-FetchContent_Declare(
-    rdma-core
-    ${FETCH_COMMAND}
-    SOURCE_DIR "${RDMA_CORE_SRC_DIR}"
-    BINARY_DIR "${RDMA_CORE_BUILD_DIR}"
+execute_process(
+    COMMAND ${CMAKE_COMMAND}
+        -S "${RDMA_CORE_SRC_DIR}"
+        -B "${RDMA_CORE_BUILD_DIR}"
+        -DNO_MAN_PAGES=1
+        -DENABLE_RESOLVE_NEIGH=0
+        -DCMAKE_SKIP_RPATH=True
+        -DNO_PYVERBS=1
 )
 
-set(NO_MAN_PAGES ON CACHE BOOL "Disable man page generation" FORCE)
-set(ENABLE_RESOLVE_NEIGH OFF CACHE BOOL "Disable resolve-neigh feature" FORCE)
-set(NO_PYVERBS ON CACHE BOOL "Disable Python bindings" FORCE)
-set(CMAKE_SKIP_RPATH ON CACHE BOOL "Disable RPATH" FORCE)
-FetchContent_MakeAvailable(rdma-core)
+execute_process(
+    COMMAND ${CMAKE_COMMAND}
+        --build "${RDMA_CORE_BUILD_DIR}"
+        --target kern-abi
+)
 
 set(RDMA_CORE_INCLUDE_DIR ${RDMA_CORE_BUILD_DIR}/include)
