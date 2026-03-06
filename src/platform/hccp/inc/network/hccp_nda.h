82d176f119a1fdbfe3e8679725055369a28a5fbd
zhongqin2020 <zhongqin3@huawei.com>
2026-03-06 18:15:40 +0800

    Add NDA API definition
    Co-authored-by: zhongqin2020<zhongqin3@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !752 merge master into master
    
    Add NDA API definition
    
    Created-by: zhongqin2020
    Commit-by: zhongqin2020
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    
    添加NDA相关功能新增接口及数据结构定义
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #123-->
    <!-- 如果这个PR不涉及Issue，可填写"NA"。-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于构造对应xx测试用例、二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [x] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/hcomm!752

---
+++ src/platform/hccp/inc/network/hccp_nda.h
@@ -0,0 +1,138 @@
+/**
+ * Copyright (c) 2026 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+#ifndef HCCP_NDA_H
+#define HCCP_NDA_H
+
+#include <infiniband/verbs.h>
+#include <stdint.h>
+#include "hccp_common.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct NdaOps {
+    void *(*alloc)(size_t size);
+    void (free)(void *ptr);
+
+    int (*memset_s)(void *dst, int value, size_t count);
+    int (*memcpy_s)(void *dst, size_t dstSize, void *src, size_t srcSize);
+};
+
+enum {
+    QBUF_DMA_MODE_DEFAULT = 0,
+    QBUF_DMA_MODE_INDEP_UB = 1,
+    QBUF_DMA_MODE_MAX = 2,
+};
+
+struct NdaCqInitAttr {
+    struct ibv_cq_init_attr_ex attr;
+
+    uint32_t dmaMode;
+    struct NdaOps *ops;
+};
+
+struct queueBuf {
+    uint64_t base;
+    uint32_t entryCnt;
+    uint32_t entrySize;
+};
+
+#ifndef _SYS_UIO_H
+struct iovec {
+    void *iov_base;
+    size_t iov_len;
+};
+#endif // _SYS_UIO_H
+
+struct queueInfo {
+    struct queueBuf qBuf;
+    struct iovec dbrPiVa;
+    struct iovec dbrCiVa;
+    struct iovec dbHwVa;
+};
+
+struct NdaCqInfo {
+    struct ibv_cq *cq;
+    struct queueInfo cqInfo;
+};
+
+struct NdaQpInitAttr {
+    struct ibv_qp_init_attr attr;
+
+    uint32_t dmaMode;
+    struct NdaOps *ops;
+};
+
+struct NdaQpInfo {
+    struct ibv_qp *qp;
+    struct queueInfo sqInfo;
+    struct queueInfo rqInfo;
+};
+
+enum {
+    DIRECT_FLAG_NOTSUPP = 0,
+    DIRECT_FLAG_PCIE = 1,
+    DIRECT_FLAG_UB = 2,
+};
+
+/**
+ * @ingroup libinit
+ * @brief Get NDA direct flag
+ * @param rdmaHandle [IN] rdma handle
+ * @param directFlag [OUT] direct flag
+ * @see RaRdevInit
+ * @retval #zero Success
+ * @retval #non-zero Failure
+*/
+HCCP_ATTRI_VISI_DEF int RaNdaGetDirectFlag(void *rdmaHandle, int *directFlag);
+
+/**
+ * @ingroup librdma
+ * @brief Create NDA cq(only one cq handle)
+ * @param rdmaHandle [IN] rdma handle
+ * @param attr [IN] NDA cq attr
+ * @param info [OUT] NDA cq info
+ * @param cqHandle [OUT] NDA cq handle
+ * @see RaNdaCqDestroy
+ * @retval #zero Success
+ * @retval #non-zero Failure
+*/
+HCCP_ATTRI_VISI_DEF int RaNdaCqCreate(void *rdmaHandle, struct NdaCqInitAttr *attr, struct NdaCqInfo *info,
+    void **cqHandle);
+
+/**
+ * @ingroup librdma
+ * @brief Destroy NDA cq(only one cq handle)
+ * @param cqHandle [IN] NDA cq handle
+ * @see RaNdaCqCreate
+ * @retval #zero Success
+ * @retval #non-zero Failure
+*/
+HCCP_ATTRI_VISI_DEF int RaNdaCqDestroy(void *cqHandle);
+
+/**
+ * @ingroup librdma
+ * @brief Create NDA qp handle(only one qp handle)
+ * @param rdmaHandle [IN] rdma handle
+ * @param attr [IN] NDA qp attr
+ * @param info [OUT] NDA qp info
+ * @param qpHandle [OUT] NDA qp handle
+ * @see RaQpDestroy
+ * @retval #zero Success
+ * @retval #non-zero Failure
+*/
+HCCP_ATTRI_VISI_DEF int RaNdaQpCreate(void *rdmaHandle, struct NdaQpInitAttr *attr, struct NdaQpInfo *info,
+    void **qpHandle);
+#ifdef __cplusplus
+}
+#endif
+#endif // HCCP_NDA_H
